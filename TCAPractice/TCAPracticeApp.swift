8f20d4165b0de24d58ff9e6802fc0352ea371d40
yunuki <woogie_ing@naver.com>
2023-09-02 21:50:33 +0900

    Initial Commit

---
+++ TCAPractice/TCAPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPracticeApp.swift
+//  TCAPractice
+//
+//  Created by 윤재욱 on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct TCAPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
